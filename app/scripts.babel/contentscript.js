09b75b2b21e469b244a23c870b757839bf01a0dd
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2017-11-11 15:27:16 -0500

    fixed issue with strings starting or ending with i

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -9,6 +9,13 @@ window.addEventListener('load', () => {
 
       if(node.nodeType == Element.TEXT_NODE){
         node.textContent = node.textContent.replace(/\ i\ /g, ' A [?] ');
+        if(node.textContent.charAt(0) == 'i') {
+          node.textContent = node.textContent.replace(/i/, 'A [?]');
+        }
+        if(node.textContent.charAt(node.textContent.length - 1) == 'i') {
+          node.textContent = node.textContent.substring(0, node.textContent.length - 2);
+          node.textContent = node.textContent + 'A [?]';
+        }
       } else if(node.nodeType == Element.ELEMENT_NODE){
         //  Check this node's child nodes for text nodes to act on
 
@@ -16,8 +23,12 @@ window.addEventListener('load', () => {
       }
     }
 
-    setTimeout(replaceTextInNode, 1000, parentNode);
+    setTimeout(replaceTextInNode, 5000, parentNode);
   };
 
   replaceTextInNode(document.body);
 });
+
+String.prototype.replaceAt = function(index, replacement) {
+    return this.substr(0, index) + replacement+ this.substr(index + replacement.length);
+}
