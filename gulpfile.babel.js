8fe065850acd8727a9de78abbb3d7b42e267f3f0
Michael Chan-Pong <Michael.Chan-Pong@outlook.com>
2017-11-11 15:33:38 -0500

    changed name of extension

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -124,7 +124,7 @@ gulp.task('wiredep', () => {
 gulp.task('package', function () {
   var manifest = require('./dist/manifest.json');
   return gulp.src('dist/**')
-      .pipe($.zip('iPhoneify-' + manifest.version + '.zip'))
+      .pipe($.zip('APhoneify-' + manifest.version + '.zip'))
       .pipe(gulp.dest('package'));
 });
 
